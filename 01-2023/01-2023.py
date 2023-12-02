5f8994e4e17eb12c02797c44dbe1a1797a80e335
christian-ae <144706250+christian-ae@users.noreply.github.com>
2023-12-02 12:12:28 +0000

    Part2

--- 01-2023/01-2023.py
+++ 01-2023/01-2023.py
@@ -1,13 +1,25 @@
 import numpy as np
 
-input = np.loadtxt('input.txt', dtype=str)
+input = np.loadtxt('input2.txt', dtype=str)
+
+# Part 2
+spelled_ints = ["one","two","three","four","five","six","seven","eight","nine"]
+convert_int = ["1","2","3","4","5","6","7","8","9"]
 
 sum = 0
 for str in input:
     char_list = []
-    for char in str:
+    substring = ""
+    for i, char in enumerate(str):
         if char.isnumeric():
             char_list.append(char)
-    sum += int(char_list[0]+char_list[-1])
+        else:
+            substring += char
+        for i, spelled_int in enumerate(spelled_ints):
+            if spelled_int in substring:
+                char_list.append(convert_int[i])
+                substring = substring[-1]
+    sum += int(char_list[0] + char_list[-1])
     char_list.clear()
+
 print(sum)
